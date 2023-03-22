967f7f3ddb632dddff88a230214c099eb4e10f12
Serge-Moskalenko <skmos2019@outlook.com>
2023-03-22 12:51:11 +0000

    finish

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const validateBody = require("./validateBody");
 const isValidId = require("./isValidId");
-const authorizationUser=require("./authorization")
+const authorizationUser = require("./authorization");
+const upload = require("./upload");
 
 module.exports = {
     validateBody,
     isValidId,
-    authorizationUser
+    authorizationUser,
+    upload,
 }
